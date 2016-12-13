a12e775389f261e3d3378e772d4fedee1445a345
unknown <>
2016-12-13 22:18:02 +0800

    [fix] 修复记录获取数据bug

--- app/src/main/java/com/tiyujia/homesport/common/record/fragment/RecordFragment.java
+++ app/src/main/java/com/tiyujia/homesport/common/record/fragment/RecordFragment.java
@@ -7,20 +7,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.api.Api;
 import com.lzy.okgo.OkGo;
 import com.tiyujia.homesport.API;
 import com.tiyujia.homesport.BaseFragment;
@@ -34,12 +27,7 @@ import com.tiyujia.homesport.entity.LoadCallback;
 import com.tiyujia.homesport.entity.LzyResponse;
 import com.tiyujia.homesport.entity.view.LoopView;
 import com.tiyujia.homesport.entity.view.OnItemSelectedListener;
-
 import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -56,10 +44,13 @@ public class RecordFragment extends BaseFragment implements View.OnClickListener
     private View ivSelect;
     private RelativeLayout.LayoutParams layoutParams;
     private Integer levelid;
+    private RelativeLayout rootview;
+    private View wheelview;
 
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.record_fragment,null);
+        wheelview=inflater.inflate(R.layout.wheelview,null);
         return view;
     }
 
@@ -70,15 +61,16 @@ public class RecordFragment extends BaseFragment implements View.OnClickListener
         tvTop=(TextView)view.findViewById(R.id.tvTop);
         tvRecord=(TextView)view.findViewById(R.id.tvRecord);
         tvSportTimes=(TextView)view.findViewById(R.id.tvSportTimes);
-        tvName=(TextView)view.findViewById(R.id.tvName);
         tvTotalScore=(TextView)view.findViewById(R.id.tvTotalScore);
         llTrack=(LinearLayout)view.findViewById(R.id.llTrack);
         rlJumpToMap= (RelativeLayout) view.findViewById(R.id.rlJumpToMap);
+        rootview = (RelativeLayout) wheelview.findViewById(R.id.rootview);
         ivSelect=view.findViewById(R.id.ivSelect);
         tvTop.setOnClickListener(this);
         tvRecord.setOnClickListener(this);
         llTrack.setOnClickListener(this);
         rlJumpToMap.setOnClickListener(this);
+        ivSelect.setOnClickListener(this);
         setData();
     }
 
@@ -124,12 +116,12 @@ public class RecordFragment extends BaseFragment implements View.OnClickListener
                 break;
             case R.id.rlJumpToMap:
                 Intent intent=new Intent(getActivity(),CityMapActivity.class);
-                startActivity(intent);
+                getActivity().startActivityForResult(intent,11);
                 break;
             case R.id.ivSelect:
                 layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-                final RelativeLayout rootview = (RelativeLayout) view.findViewById(R.id.rootview);
+
                 final LoopView loopView = new LoopView(getActivity());
                 if(levelid!=null){
                     OkGo.post(API.BASE_URL+"/v2/record/sportinfo/level")
@@ -140,26 +132,27 @@ public class RecordFragment extends BaseFragment implements View.OnClickListener
                                 public void onSuccess(LevelModel levelModel, Call call, Response response) {
                                     if (levelModel.state==200){
                                         ArrayList<String> list = new ArrayList<>();
-                                        for (int i=0;i<levelModel.data.size();i++){
-                                            LevelModel.Level jk = levelModel.data.get(i);
-                                            list.add(jk.level);
-                                        }
-                                        //滚动监听
-                                        loopView.setListener(new OnItemSelectedListener() {
-                                            @Override
-                                            public void onItemSelected(int index) {
-
+                                        if(levelModel.data.size()>0){
+                                            for (int i=0;i<levelModel.data.size();i++){
+                                                LevelModel.Level jk = levelModel.data.get(i);
+                                                list.add(jk.level);
                                             }
-                                        });
-                                        //设置原始数据
-                                        loopView.setItems(list);
-                                        //设置初始位置
-                                        loopView.setInitPosition(5);
-                                        //设置字体大小
-                                        loopView.setTextSize(10);
-                                        rootview.addView(loopView, layoutParams);
-                                    }
+                                            //滚动监听
+                                            loopView.setListener(new OnItemSelectedListener() {
+                                                @Override
+                                                public void onItemSelected(int index) {
 
+                                                }
+                                            });
+                                            //设置原始数据
+                                            loopView.setItems(list);
+                                            //设置初始位置
+                                            loopView.setInitPosition(5);
+                                            //设置字体大小
+                                            loopView.setTextSize(10);
+                                            rootview.addView(loopView, layoutParams);
+                                        }else {}
+                                    }
                                 }
                             });
                 }else {}
@@ -172,13 +165,12 @@ public class RecordFragment extends BaseFragment implements View.OnClickListener
     @Override
     public void onResume() {
         super.onResume();
+        tvName=(TextView)view.findViewById(R.id.tvName);
         SharedPreferences city=getActivity().getSharedPreferences("City", Context.MODE_PRIVATE);
         String name=city.getString("name","");
         levelid=city.getInt("id",0);
         if(!TextUtils.isEmpty(name)){
             tvName.setText(name);
         }else {}
-
-
     }
 }
