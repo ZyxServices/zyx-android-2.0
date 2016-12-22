218e5a8994eb95fde4d36b6d65633390af5f373a
Cymbi <928902646@qq.com>
2016-12-22 20:20:29 +0800

    [updata] 修改ignore

--- app/src/main/java/com/tiyujia/homesport/common/personal/adapter/EquipmentShowAdapter.java
+++ app/src/main/java/com/tiyujia/homesport/common/personal/adapter/EquipmentShowAdapter.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 /**
  * 作者: Cymbi on 2016/12/7 12:08.
- * 邮箱:928902646@qq.com
+ * 邮箱:928902646@qq.comcxzczcxcxcxz
  */
 
 public class EquipmentShowAdapter extends BaseQuickAdapter<EquipmentShowModel.Model> {
